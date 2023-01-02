81ca5cb833ccf3e7758e6ae3de9df51af3a83aa9
dominic-source <chinonsodomnic@gmail.com>
2023-01-02 16:59:23 +0100

    debug V3

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './scss/mobile.scss';
+import './scss/App.scss';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
